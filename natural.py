3b8418009232028b269d17e85c6cb7ecd02cc11c
Mohamed Sathik <satikofficial2028@gmail.com>
2023-12-26 20:05:48 +0530

    Updated in While and range

--- natural.py
+++ natural.py
@@ -2,3 +2,13 @@ num = int(input("Enter your number:"))
 
 for i in range (1, num+1):
     print(i, end=" ,")
+
+
+
+####################################################
+
+number = int(input("Enter the Value:"))
+i = 1
+while i < 6:
+    i += 1
+    print(i)
