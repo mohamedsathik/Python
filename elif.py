ba49977faf208c0dd759f30c98f8341eeb56693b
mohamedsathik <sathikofficial2028@gmail.com>
2023-12-30 19:53:23 +0530

    Postive or Negative

---
+++ elif.py
@@ -0,0 +1,10 @@
+num = int(input("Enter the Number : "))
+
+if num > 0:
+    print("The number is Positive")
+
+elif num < 0:
+    print("The number is Negative")
+
+else:
+    print("The number is Zero")
