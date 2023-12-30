d395e8c41ef065cd6e8258fdfe0f5feb3145ab14
mohamedsathik <sathikofficial2028@gmail.com>
2023-12-30 20:06:22 +0530

    All Operation

---
+++ maths.py
@@ -0,0 +1,18 @@
+a = int(input("Enter the First Number : "))
+b = int(input("Enter the Second Number : "))
+
+sum = a + b
+sub = a - b
+product = a * b
+divide = a / b
+flor_quo = a // b
+mod = a % b
+power = pow(a,b)
+
+print ("sum = ",sum) 
+print ("sub = ",sub) 
+print("product = ",product) 
+print("divide = ",divide) 
+print("flor_quo = ",flor_quo) 
+print("mod = ",mod) 
+print("power = ",power)
