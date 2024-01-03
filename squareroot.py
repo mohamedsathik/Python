70182ebd4b2cc70f2a22e2c348efd9fbc467fe8b
mohamedsathik <sathikofficial2028@gmail.com>
2024-01-03 14:10:07 +0530

    Square and cube root of value

---
+++ squareroot.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the First number : "))
+import math
+
+sqrt = math.sqrt(a)
+square = a ** 2
+cube = a ** (1/3)
+print("\nThe square root of", a, "is: ", sqrt)
+print("\nThe Square of", a,"is : ", square)
+print("\n The Cube of ", a, "is :", cube )
