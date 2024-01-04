89db7a5ab7b62e214a2947bf92c3ab15107cb7c2
mohamedsathik <sathikofficial2028@gmail.com>
2024-01-04 11:55:04 +0530

    Greater of three value

---
+++ Greatofthree.py
@@ -0,0 +1,7 @@
+first = int(input("Enter the number : "))
+Second = int(input("Enter the second number : "))
+third =  int(input("Enter the Third Number : "))
+
+result = max(first ,max(Second,third))
+
+print(result)
