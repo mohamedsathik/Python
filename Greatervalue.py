84ffcbf507df9fcb0389430d6b9c7aafdd883af3
mohamedsathik <sathikofficial2028@gmail.com>
2024-01-04 11:46:23 +0530

    Find the Greater value

---
+++ Greatervalue.py
@@ -0,0 +1,7 @@
+First = int(input("Enter the First Number: "))
+Second = int(input("Enter the second Number: "))
+
+if First==Second:
+    print ("Both numbers are equal")
+else:
+    print(max(First,Second), "is greater")
